665104b52379eb184546763c775f881ce20d9b4e
Eric Zhang <ezhang887@gmail.com>
2019-01-07 19:15:56 -0800

    simplify trajectory length

--- trajectory/motion_profile.go
+++ trajectory/motion_profile.go
@@ -1,26 +1,26 @@
 package trajectory;
 
 import (
+    "fmt"
     "bytes"
 )
 
 type MotionProfile struct{
     points []Point
-    length int
 }
 
 func (this *MotionProfile) Length() int {
-    return this.length;
+    return len(this.points)
 }
 
 func AddPoint(mp *MotionProfile, point *Point) {
     mp.points = append(mp.points, *point)
-    mp.length += 1
 }
 
 func (this *MotionProfile) String() string{
     var buffer bytes.Buffer
 
+    buffer.WriteString(fmt.Sprintf("Total length: %d", this.Length()))
     buffer.WriteString("(Time, Pos, Vel, Accel)\n")
 
     for _, point  := range this.points{
